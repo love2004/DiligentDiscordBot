e216f7bb5042fd71973ffa7c3b869981fc1d9d59
NUTT1101 <zhangf099@gmail.com>
2023-12-23 17:12:57 +0800

    c

--- src/test/java/org/dyu5thdorm/dyu5thdormdiscordbot/Dyu5thDormDiscordBotApplicationTests.java
+++ src/test/java/org/dyu5thdorm/dyu5thdormdiscordbot/Dyu5thDormDiscordBotApplicationTests.java
@@ -8,6 +8,11 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintStream;
+import java.util.*;
+
 @SpringBootTest
 class Dyu5thDormDiscordBotApplicationTests {
     @Autowired
@@ -37,12 +42,32 @@ class Dyu5thDormDiscordBotApplicationTests {
     ActivityPartiRepo activitiesPartiRepo;
     @Autowired
     ActivityService activityService;
+    @Autowired
+    TicketViewRepo ticketViewRepo;
 
     @Test
-    void contextLoads() {
-        System.out.println(
-                activityService.findActivitiesActive()
-        );
+    void contextLoads() throws FileNotFoundException {
+        Scanner scanner = new Scanner(new File("tickets"));
+        List<String> ticketBox = new ArrayList<>();
+        while (scanner.hasNextLine()) {
+            ticketBox.add(scanner.next());
+        }
+        Collections.shuffle(ticketBox);
+
+        Random random = new Random();
+        List<String> winners = new ArrayList<>();
+        PrintStream stream = new PrintStream("winner.txt");
+        int count = 0;
+        while (count < 100000) {
+            String winner = ticketBox.get(random.nextInt(ticketBox.size()));
+            if (winners.contains(winner)) continue;
+            else winners.add(winner);
+            if (winners.size() == 3) {
+                stream.println(String.join(",", winners));
+                winners.clear();
+                count++;
+            }
+        }
     }
 
 }
